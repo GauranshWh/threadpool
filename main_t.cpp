be7fa313ca19207232c6bfd32392796c3995db93
GauranshWh <86873677+GauranshWh@users.noreply.github.com>
2025-07-23 19:27:28 +0530

    Update main_t.cpp

--- main_t.cpp
+++ main_t.cpp
@@ -5,25 +5,32 @@
 
 std::mutex cout_mutex;
 
+int main() {
+    ThreadPool pool(4, 1000);
+    const int num_tasks = 400;
+    auto start = std::chrono::high_resolution_clock::now();
 
-int main(){
-    ThreadPool pool(5,500);
-
-    for(int i = 0 ; i < 400 ;++i){
-        pool.enqueue([i]{
+    for (int i = 0; i < num_tasks; ++i) {
+        pool.enqueue([i] {
             {
                 std::lock_guard<std::mutex> lock(cout_mutex);
-                std::cout<< " Tast" << i << "executed by thread" << std::this_thread::get_id()<<std::endl;
+                std::cout << "Task " << i << " executed by thread "
+                          << std::this_thread::get_id() << std::endl;
             }
-           
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
         });
-
     }
 
-    std:: this_thread::sleep_for(std::chrono::seconds(2));
     pool.shutdown();
+
+    //Total time taken
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "Completed " << num_tasks << " tasks in "
+                  << duration.count() << " seconds" << std::endl;
+    }
 
     return 0;
 }
